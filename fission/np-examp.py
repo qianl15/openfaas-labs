410791b4f2da3655954c5079da447bed8345ebe4
faromero <faromero@stanford.edu>
2018-06-19 04:50:13 +0000

    Example now demonstrates how to use Google Cloud Storage

--- fission/np-examp.py
+++ fission/np-examp.py
@@ -4,6 +4,8 @@ from flask import current_app
 
 import numpy as np
 
+from google.cloud import storage
+
 # Python env doesn't pass in an argument
 # we can get request data by request.headers, request.get_data()
 def main():
@@ -15,7 +17,29 @@ def main():
     z = np.matmul(x,y)
     zz = np.array2string(z)
 
-    msg2 = '--RESULT--\n{}\n-----\n'.format(zz)
+    fin_res = ''
+
+    client = storage.Client()
+    bucket_fail = False
+    try:
+      bucket = client.get_bucket('fission-bucket')
+    except:
+      bucket_fail = True
+
+    print("Checking bucket...")
+    if not bucket_fail:
+      print("Bucket didn't fail")
+      blob = bucket.get_blob('aws_prop.txt')
+      if blob == None:
+        fin_res = 'Bucket found, blob not found'
+      else:
+        print("Getting blob...")
+        fin_res = blob.download_as_string()
+    else:
+      print("Bucket failed")
+      fin_res = 'Bucket not found'
+
+    msg2 = '--RESULT--\n{}\n{}\n-----\n'.format(zz, fin_res)
     msg += msg2
 
     current_app.logger.info(msg)
